b7a5c6d67187e0338c3607d460d2a3a2a6bbea97
Jakub <xchrappaj@stuba.sk>
2022-04-18 11:50:59 +0200

    Zatial len test podla cvika

---
+++ src/main/java/sk/stuba/fei/uim/oop/assignment3/logika/TestSpring.java
@@ -0,0 +1,14 @@
+package sk.stuba.fei.uim.oop.assignment3.logika;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestSpring {
+
+    public TestSpring() {
+
+        System.out.println("Ahoj svet");
+
+    }
+}
