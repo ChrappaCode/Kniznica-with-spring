6e59338eab04275f69e20f4f96c70fd7394284bc
Jakub <xchrappaj@stuba.sk>
2022-04-18 15:54:16 +0200

    restcontoler ahoj (get)

--- src/main/java/sk/stuba/fei/uim/oop/assignment3/web/AhojKontroler.java
+++ src/main/java/sk/stuba/fei/uim/oop/assignment3/web/AhojKontroler.java
@@ -1,14 +1,34 @@
 package sk.stuba.fei.uim.oop.assignment3.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
 
 @RestController
 public class AhojKontroler {
 
-    @GetMapping("/ahoj")
+    private int pocitadlo = 0;
+
+    @GetMapping(value = "/ahoj")
     public String ahoj(){
-        return "ahoj";
+        return "ahoj " + (this.pocitadlo++);
+    }
+
+    @GetMapping(value = "/param")
+    public String ahojParameter(@RequestParam(name = "name" , defaultValue = "skap") String meno){
+
+        return "ahoj " + meno;
+
+    }
+
+    @GetMapping(value = "/path/{name}")
+    public String ahojPathParameter(@PathVariable("name") String meno){
+
+        return "ahoj " + meno;
+
     }
 
 }
---
+++ src/main/java/sk/stuba/fei/uim/oop/assignment3/web/AhojOdozva.java
@@ -0,0 +1,11 @@
+package sk.stuba.fei.uim.oop.assignment3.web;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class AhojOdozva {
+
+    private int cislo;
+    private String sprava;
+
+}
