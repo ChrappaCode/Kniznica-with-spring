df6f889620738ce48dd67650d2f3177a76bedb90
Jakub <xchrappaj@stuba.sk>
2022-04-22 12:00:36 +0200

    zaciatok knihy+

--- src/main/java/sk/stuba/fei/uim/oop/assignment3/book/service/BookService.java
+++ src/main/java/sk/stuba/fei/uim/oop/assignment3/book/service/BookService.java
@@ -3,6 +3,7 @@ package sk.stuba.fei.uim.oop.assignment3.book.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import sk.stuba.fei.uim.oop.assignment3.author.author.Author;
+import sk.stuba.fei.uim.oop.assignment3.author.author.AuthorRepository;
 import sk.stuba.fei.uim.oop.assignment3.book.book.Book;
 import sk.stuba.fei.uim.oop.assignment3.book.book.BookRepository;
 import sk.stuba.fei.uim.oop.assignment3.book.web.BookRequest;
@@ -15,6 +16,8 @@ public class BookService implements IBookService{
 
     @Autowired
     private BookRepository repository;
+    @Autowired
+    private AuthorRepository repository1;
 
     @Override
     public List<Book> getAllBooks() {
@@ -23,7 +26,14 @@ public class BookService implements IBookService{
 
     @Override
     public Book create(BookRequest request) throws NotFound {
+
+        if (this.repository1.findAuthorById(request.getAuthorId()) == null) {
+            throw new NotFound();
+        }
+        Author a = this.repository1.findAuthorById(request.getAuthorId());
+
         Book newBook = new Book();
+
         newBook.setName(request.getName());
         newBook.setDescription(request.getDescription());
         newBook.setAuthor(request.getAuthorId());
